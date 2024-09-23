56e67151cda4f96112b38cf18e875502dcfabf43
harshit Mishra <miananya2@gmail.com>
2024-09-24 03:08:51 +0530

    Billboard

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,5 +1,18 @@
-const HomePage = () => {
-  return <div>Home Page</div>;
+import { Billboard } from "@/components/Billboard";
+import { Container } from "@/components/ui/Container";
+import { getBillboard } from "../actions/getBillboard";
+
+export const revalidate = 0;
+
+const HomePage = async () => {
+  const billboard = await getBillboard("76d5722c-262d-473f-b18f-fb73907516a5");
+  return (
+    <Container>
+      <div className="space-y-10 pb-10">
+        <Billboard data={billboard} />
+      </div>
+    </Container>
+  );
 };
 
 export default HomePage;
